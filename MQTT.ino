cb18401ae80d4f5449351a4daf7361a6dda203ad
misterknister66 <YOUR_EMAIL>
2026-04-23 01:29:39 +0200

    Revert "Add files via upload"
    This reverts commit 9f444042d659efd259c65abc5f987c26f15420de.

--- MQTT.ino
+++ MQTT.ino
@@ -1,38 +1,39 @@
 bool mqttConnect() {   // 
 /* this function checks if we are connected to the broker, if not connect anyway */  
-   
     if( MQTT_Client.connected() ) {
     consoleOut("mqtt was connected");
     return true;
     }
-     consoleOut(F("\nconnect mqtt"));
     // we are here because w'r not connected. Signal with the LED
-    //ledblink(2,70);
+    consoleOut("mqtt connecting");
+    ledblink(2,70);
 
-    //if (Mqtt_Port == 0 ) { Mqtt_Port = 1883;}   // just in case ....
+    if (Mqtt_Port[0] == '\0' ) strcpy(Mqtt_Port, "1883");   // just in case ....
     uint8_t retry = 3;
     
-    //char Mqtt_inTopic[11]={"ESP-ECU/in"};
+    //String Clientid = getChipId(false); 
 
     while (!MQTT_Client.connected()) {
 
-      if ( MQTT_Client.connect( settings.Mqtt_Clientid, settings.Mqtt_Username, settings.Mqtt_Password) )
+      if ( MQTT_Client.connect( getChipId(false).c_str(), Mqtt_Username, Mqtt_Password) )
       {
          //connected, so subscribe to inTopic (not for thingspeak)
-        if( settings.Mqtt_Format != 0 ) {
-           //String sub = "ESP32-" + getChipId(true) + "/in"; // to get a intopic ESP32-234567/in
-           if( MQTT_Client.subscribe ( settings.Mqtt_inTopic ) ) {
-               consoleOut("subscribed to " + String(settings.Mqtt_inTopic));
+        if(Mqtt_Format != 5 ) {
+        String clientid = getChipId(false) + "/in"; 
+        if(  MQTT_Client.subscribe ( clientid.c_str() ) ) {
+        //if(  MQTT_Client.subscribe ( Mqtt_inTopic ) ) { 
+               //if(diagNose) ws.textAll("subscribed to " + String(Mqtt_inTopic ));
+               consoleOut("subscribed to " + clientid);
            }
         }
          consoleOut(F("mqtt connected"));
-         UpdateLog(3, "connected");
+         Update_Log(3, "connected");
       
        return true;
 
     } else {
         //String term = "connection failed state: " + String(MQTT_Client.state());
-        UpdateLog(3, "failed");
+        Update_Log(3, "failed");
         if (!--retry) break; // stop when tried 3 times
         delay(500);
     }
@@ -49,12 +50,16 @@ bool mqttConnect() {   //
 
 void MQTT_Receive_Callback(char *topic, byte *payload, unsigned int length)
 {
-    // domoticz messages can be long so the buffer should be large enough (setup)
-    int idx = 0;
-    uint8_t nLevel = 0;
-    consoleOut(F("received mqtt"));
-    JsonDocument doc;       // We use json library to parse the payload 
-    String jsonString = "";                        
+   
+//    String Payload = "";     // convert the payload to a String...
+//    for (int i = 0; i < length; i++)
+//    {
+//        Payload += (char)payload[i]; // convert to char, needed???
+//    }
+    
+   // ws.textAll("mqtt received " + Payload);
+
+    JsonDocument doc;       // We use json library to parse the payload                         
    //  The function deserializeJson() parses a JSON input and puts the result in a JsonDocument.
    // DeserializationError error = deserializeJson(doc, Payload); // Deserialize the JSON document
     DeserializationError error = deserializeJson(doc, payload); // Deserialize the JSON document
@@ -63,78 +68,59 @@ void MQTT_Receive_Callback(char *topic, byte *payload, unsigned int length)
        consoleOut("mqtt no valid json ");
         return;
     } 
-    // the json contains a lot, a.o. idx and "svalue1" : "72" and "level":72,
-    
+    consoleOut("Deserialized JSON:");
+    serializeJson(doc, Serial);     // Print in one line
+    Serial.println();
     // We check the kind of command format received with MQTT
-    //now we have a payload like {"switch",on}    
-    if(!doc["idx"].isNull())
+      // if( doc["throttle"] != 0 )
+    //if(doc.containsKey("throttle"))
+    if (!doc["throttle"].isNull())
     {
-      idx = doc["idx"].as<int>();
-      //now lookup the device
-      
-      if(idx == settings.Mqtt_switchIDX) {
-        consoleOut("there is recognized idx " + String(idx)) ;
-      } else {
-        consoleOut("wrong idx, abort");
-        return;
-      }
-    }  //consoleOut("devNr " + String(dev)) ;
+       int Invert = doc["throttle"].as<int>(); 
+       int throtVal = doc["val"].as<int>(); 
+       String term = "mqtt got message {\"throttle\":" + String(Invert) + ",\"val\":" + String(throtVal) + "}";
+       consoleOut(term);
+      if(Invert > inverterCount || Invert < 0 || throtVal > 700 || throtVal < 20 )
+         {
+         consoleOut("invalid value(s), skipping");
+         return; 
+         }
+      // write the desired throtval 
+      desiredThrottle[Invert] = throtVal;
+      //Inv_Prop[invert].maxPower = throtVal;
+      actionFlag = 240 + Invert;  
+    }  
 
-    if (!doc["Level"].isNull()) 
+    if (!doc["poll"].isNull())
     {
-      nLevel = doc["Level"].as<uint8_t>();
-    } else {
-      consoleOut("no nvalue, abort");
-      return;
-    }
-    consoleOut("mqtt nLevel = " + String(nLevel));
-    // switch the lamp without reporting to mqtt
-    set_dim_level(nLevel);
-    if(nLevel != 0)  
-    { 
-        UpdateLog(3, "dim command"); 
-    } else  { 
-        UpdateLog(3, "switched off");
-        checkTimers(); // disarm a timer that is active
-    }
-}
+      //now we have a payload like {"poll",1} 
+        int inv = doc["poll"].as<int>(); 
+        consoleOut( "got message {\"poll\":" + String(inv) + "}" );
 
-bool mqttGeldig() {
-    if (settings.Mqtt_Broker[0] == '\0' || settings.Mqtt_Format == 0) {
-    return false;
-    }
-    if (settings.Mqtt_switchIDX == 0) { // geen geldige idx
-    return false;   
-    } else {
-    return true; 
-    }
-}
+        if( !Polling && dayTime )
+        {
+             
+            //ws.textAll( "found {\"poll\" " + String(inv) + "}\"" );
+            
+            iKeuze = inv;
+            if(iKeuze == 99) {
+                actionFlag = 48; // takes care for the polling of all inverters
+                return;  
+            }
 
-void sendMqttswitch() 
-{
-  if ( !mqttGeldig() ){ 
-    consoleOut("no valid mqtt address or not configured, skipping..");
-    return;
-  }    
-// update switch state
-   char out[124];
-  JsonDocument doc;
-  doc["command"] = "switchlight";
-  doc["idx"] = settings.Mqtt_switchIDX;
-  if(settings.Mqtt_Format == 1) {
-  (dimmer_state == 0) ? doc["nvalue"] = 0 : doc["nvalue"] = 1;
-  } else {
-  (dimmer_state == 0) ? doc["state"] = "off" : doc["state"] = "on";  
-  }
-  int b =serializeJson(doc, out);
-  MQTT_Client.publish (settings.Mqtt_outTopic, out );
-
-  //{"command":"switchlight","idx":981,"switchcmd":"Set Level","level":64}'
-  JsonDocument dac;
-  dac["command"] ="switchlight";
-  dac["idx"] = settings.Mqtt_switchIDX;
-  dac["switchcmd"] = "Set Level";
-  dac["level"] = current_duty;
-  int c = serializeJson(dac, out);
-  MQTT_Client.publish (settings.Mqtt_outTopic, out );
+            if ( iKeuze < inverterCount ) 
+            { 
+              actionFlag = 220+inv; // takes care for the polling
+              return;
+            } else {
+               consoleOut("mqtt error no inv " + String(iKeuze));
+              return;         
+            }
+        }
+        else
+        {
+          consoleOut("autopolling set or nightTime, skipping");
+        }
+        consoleOut("nothing familiair found in mqtt");
+    }
 }
